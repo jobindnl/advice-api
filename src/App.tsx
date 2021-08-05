2591bbac10816c13122cc72b99826774fd7b6855
jobindnl <jobindnl@gmail.com>
2021-08-05 19:36:17 -0400

    background image and other styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import axios from 'axios'
-import { useState } from 'react'
-import './App.css';
+import RandomAdvice from './components/RandomAdvice'
+import './App.css'
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const getAdvice = async () => {
-    try {
-      setLoading(status => !status) //sets loading to true
-      const message = await axios('https://api.adviceslip.com/advice')
-      setLoading(status => !status) //sets loading to false
-      console.log(message.data.slip.advice)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
-    <div>
-      I'm here
-      {!loading ? 
-        <button onClick={getAdvice}>click here for advice</button>
-        :
-        <p>loading</p>
-      }
-      
+    <div className='random-advice-container'>
+      <RandomAdvice />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/RandomAdvice.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { Row, Col, Container, Button } from 'react-bootstrap'
+import './RandomAdvice.css'
+
+function RandomAdvice() {
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState('')
+
+  const getAdvice = async () => {
+    try {
+      setLoading(status => !status) //sets loading to true
+      const message = await axios('https://api.adviceslip.com/advice')
+      setLoading(status => !status) //sets loading to false
+      setMessage(message.data.slip.advice)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <Container className='random-advice-component 
+      d-flex 
+      flex-column 
+      align-items-center
+      justify-content-around'
+    >
+      <Row>
+        <h1 className='heading'>
+          Advice Generator
+        </h1>
+      </Row>
+      <Row>
+        <Col>
+          {
+            !loading ?
+              <Button onClick={getAdvice} className='btn-dark'>Click for Advice</Button>
+              :
+              <Button disabled className='btn-secondary'>Loading...</Button>
+          }
+        </Col>
+      </Row>
+      <Row >
+        {
+          message ?
+            <Col>
+              <em className='message p-3'>'{message}'</em>
+            </Col>
+            :
+            ''
+        }
+      </Row>
+
+    </Container>
+  )
+}
+
+export default RandomAdvice
